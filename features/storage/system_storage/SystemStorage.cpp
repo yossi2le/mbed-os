d1eddc16255a147d2c4f64d24ecfd3c39fbe0d45
Amir Cohen <the.amiro.cohen@gmail.com>
2018-08-13 11:55:15 +0300

    Add FileSystem tests

--- features/storage/system_storage/SystemStorage.cpp
+++ features/storage/system_storage/SystemStorage.cpp
@@ -7,15 +7,24 @@ MBED_WEAK BlockDevice* get_default_blockdevice()
 {
 #if COMPONENT_SPIF
 
-    static SPIFBlockDevice default_bd(MBED_CONF_SPIF_DRIVER_SPI_MOSI, MBED_CONF_SPIF_DRIVER_SPI_MISO,
-        MBED_CONF_SPIF_DRIVER_SPI_CLK, MBED_CONF_SPIF_DRIVER_SPI_CS, MBED_CONF_SPIF_DRIVER_SPI_FREQ);
+    static SPIFBlockDevice default_bd(
+        MBED_CONF_SPIF_DRIVER_SPI_MOSI, 
+        MBED_CONF_SPIF_DRIVER_SPI_MISO,
+        MBED_CONF_SPIF_DRIVER_SPI_CLK, 
+        MBED_CONF_SPIF_DRIVER_SPI_CS, 
+        MBED_CONF_SPIF_DRIVER_SPI_FREQ
+        );
 
     return &default_bd;
 
 #elif COMPONENT_SD
 
-    static SDBlockDevice default_bd(MBED_CONF_SD_SPI_MOSI, MBED_CONF_SD_SPI_MISO,
-                MBED_CONF_SD_SPI_CLK, MBED_CONF_SD_SPI_CS);;
+    static SDBlockDevice default_bd(
+        MBED_CONF_SD_SPI_MOSI, 
+        MBED_CONF_SD_SPI_MISO,
+        MBED_CONF_SD_SPI_CLK, 
+        MBED_CONF_SD_SPI_CS
+        );
 
     return &default_bd;
 
